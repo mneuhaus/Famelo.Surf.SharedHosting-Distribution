bee3c3e65b5cc3ec6e1ba503b0f562604b4836c4
Marc Neuhaus <apocalip@gmail.com>
2013-08-15 00:38:11 +0200

    updated df deployment

--- Build/Surf/deployment.famelo.com.php
+++ Build/Surf/deployment.famelo.com.php
@@ -6,7 +6,7 @@ use \TYPO3\Surf\Domain\Model\SimpleWorkflow;
 
 $application = new \Famelo\Surf\SharedHosting\Application\Flow('deployment.famelo.com');
 $application->setDeploymentPath('/kunden/350350_33330/deployment');
-$application->setOption('repositoryUrl', 'git@github.com:mneuhaus/Famelo.Brain-Distribution.git');
+$application->setOption('repositoryUrl', 'git@github.com:mneuhaus/Famelo.Surf.SharedHosting-Distribution.git');
 
 $node = new Node('deployment.famelo.com');
 $node->setHostname('famelo.com');
@@ -22,7 +22,7 @@ $application->setHosting('DomainFactory/ManagedHosting');
 
 $workflow = new SimpleWorkflow();
 // Enable to keep "broken" deployment for aftermath analysis
-// $workflow->setEnableRollback(FALSE);
+$workflow->setEnableRollback(FALSE);
 
 
 // Download Beard and patch based on beard.json or gerrit.json
