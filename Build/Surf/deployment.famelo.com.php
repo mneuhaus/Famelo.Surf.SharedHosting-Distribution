ef9d6f8aa74c7e0a8206cf320943a12de45ef6cf
Marc Neuhaus <apocalip@gmail.com>
2013-08-15 00:29:39 +0200

    initial commit

---
+++ Build/Surf/deployment.famelo.com.php
@@ -0,0 +1,36 @@
+<?php
+use \TYPO3\Surf\Domain\Model\Workflow;
+use \TYPO3\Surf\Domain\Model\Node;
+use \TYPO3\Surf\Domain\Model\SimpleWorkflow;
+
+
+$application = new \Famelo\Surf\SharedHosting\Application\Flow('deployment.famelo.com');
+$application->setDeploymentPath('/kunden/350350_33330/deployment');
+$application->setOption('repositoryUrl', 'git@github.com:mneuhaus/Famelo.Brain-Distribution.git');
+
+$node = new Node('deployment.famelo.com');
+$node->setHostname('famelo.com');
+$node->setOption('username', 'ssh-350350-famelo');
+$application->addNode($node);
+
+// Specify the hosting package
+$application->setHosting('DomainFactory/ManagedHosting');
+
+// Set the default context
+// $application->setContext('Production');
+// $application->setOption('keepReleases', 1);
+
+$workflow = new SimpleWorkflow();
+// Enable to keep "broken" deployment for aftermath analysis
+// $workflow->setEnableRollback(FALSE);
+
+
+// Download Beard and patch based on beard.json or gerrit.json
+// $workflow->afterTask('typo3.surf:composer:install', array(
+// 	'famelo.surf.sharedhosting:downloadBeard',
+// 	'famelo.surf.sharedhosting:beardPatch'
+// ));
+
+$deployment->setWorkflow($workflow);
+$deployment->addApplication($application);
+?>
---
+++ Configuration/PackageStates.php
@@ -0,0 +1,186 @@
+<?php
+# PackageStates.php
+
+# This file is maintained by Flow's package management. Although you can edit it
+# manually, you should rather use the command line commands for maintaining packages.
+# You'll find detailed information about the typo3.flow:package:* commands in their
+# respective help screens.
+
+# This file will be regenerated automatically if it doesn't exist. Deleting this file
+# should, however, never become necessary if you use the package commands.
+
+return array (
+  'packages' => 
+  array (
+    'Composer.Installers' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'composer/installers',
+      'state' => 'active',
+      'packagePath' => 'Libraries/composer/installers/',
+      'classesPath' => 'src/',
+    ),
+    'doctrine.cache' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/cache',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/cache/',
+      'classesPath' => 'lib/',
+    ),
+    'doctrine.collections' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/collections',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/collections/',
+      'classesPath' => 'lib/',
+    ),
+    'doctrine.inflector' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/inflector',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/inflector/',
+      'classesPath' => 'lib/',
+    ),
+    'doctrine.lexer' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/lexer',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/lexer/',
+      'classesPath' => 'lib/',
+    ),
+    'doctrine.annotations' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/annotations',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/annotations/',
+      'classesPath' => 'lib/',
+    ),
+    'Doctrine.Common' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/common',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/common/',
+      'classesPath' => 'lib/',
+    ),
+    'Doctrine.DBAL' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/dbal',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/dbal/',
+      'classesPath' => 'lib/',
+    ),
+    'doctrine.migrations' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/migrations',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/migrations/',
+      'classesPath' => 'lib/',
+    ),
+    'symfony.console' => 
+    array (
+      'manifestPath' => 'Symfony/Component/Console/',
+      'composerName' => 'symfony/console',
+      'state' => 'active',
+      'packagePath' => 'Libraries/symfony/console/',
+      'classesPath' => '',
+    ),
+    'Doctrine.ORM' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'doctrine/orm',
+      'state' => 'active',
+      'packagePath' => 'Libraries/doctrine/orm/',
+      'classesPath' => 'lib/',
+    ),
+    'symfony.domcrawler' => 
+    array (
+      'manifestPath' => 'Symfony/Component/DomCrawler/',
+      'composerName' => 'symfony/dom-crawler',
+      'state' => 'active',
+      'packagePath' => 'Libraries/symfony/dom-crawler/',
+      'classesPath' => '',
+    ),
+    'symfony.yaml' => 
+    array (
+      'manifestPath' => 'Symfony/Component/Yaml/',
+      'composerName' => 'symfony/yaml',
+      'state' => 'active',
+      'packagePath' => 'Libraries/symfony/yaml/',
+      'classesPath' => '',
+    ),
+    'TYPO3.Flow' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/flow',
+      'state' => 'active',
+      'packagePath' => 'Framework/TYPO3.Flow/',
+      'classesPath' => 'Classes/',
+    ),
+    'TYPO3.Fluid' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/fluid',
+      'state' => 'active',
+      'packagePath' => 'Framework/TYPO3.Fluid/',
+      'classesPath' => 'Classes/',
+    ),
+    'TYPO3.Party' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/party',
+      'state' => 'active',
+      'packagePath' => 'Framework/TYPO3.Party/',
+      'classesPath' => 'Classes/',
+    ),
+    'Famelo.Surf.SharedHosting' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'famelo/surf-sharedhosting',
+      'state' => 'active',
+      'packagePath' => 'Application/Famelo.Surf.SharedHosting/',
+      'classesPath' => 'Classes/',
+    ),
+    'TYPO3.Kickstart' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/kickstart',
+      'state' => 'active',
+      'packagePath' => 'Framework/TYPO3.Kickstart/',
+      'classesPath' => 'Classes/',
+    ),
+    'TYPO3.Surf' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/surf',
+      'state' => 'active',
+      'packagePath' => 'Application/TYPO3.Surf/',
+      'classesPath' => 'Classes/',
+    ),
+    'TYPO3.Welcome' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'typo3/welcome',
+      'state' => 'active',
+      'packagePath' => 'Framework/TYPO3.Welcome/',
+      'classesPath' => 'Classes/',
+    ),
+    'mikey179.vfsStream' => 
+    array (
+      'manifestPath' => '',
+      'composerName' => 'mikey179/vfsStream',
+      'state' => 'active',
+      'packagePath' => 'Libraries/mikey179/vfsStream/',
+      'classesPath' => 'src/main/php/',
+    ),
+  ),
+  'version' => 4,
+)
+ ?>
